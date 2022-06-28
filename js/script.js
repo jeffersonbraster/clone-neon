554e3debfca39909fdf187f38c2da6da651781b4
Jefferson <jeferson.oliveira@g.globo>
2022-06-27 21:14:21 -0300

    criando menu mobile

--- js/script.js
+++ js/script.js
@@ -22,3 +22,13 @@ function fixedMenu() {
 }
 
 document.addEventListener("scroll", fixedMenu);
+
+const btnMenu = document.getElementById("js-btn-menu-mobile");
+const overlayMenu = document.querySelector(".js-overlay");
+
+function openMenuMobile() {
+  document.documentElement.classList.toggle("menu-opened");
+}
+
+btnMenu.addEventListener("click", openMenuMobile);
+overlayMenu.addEventListener("click", openMenuMobile);
